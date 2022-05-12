c23cd9db7175da5e4a2b632808449b17ecd0e0d3
Simha Marouani <simhama@std.hac.ac.il>
2022-05-12 10:22:56 +0300

    /

---
+++ Final project/Final project.cpp
@@ -0,0 +1,12 @@
+﻿// Final project.cpp : Defines the entry point for the application.
+//
+//
+#include "Final project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
