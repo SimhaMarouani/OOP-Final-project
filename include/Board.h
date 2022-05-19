9fe6e3c7daf38ede48b0cc5446f70e9f27a9d5dd
Simha Marouani <simhama@std.hac.ac.il>
2022-05-19 12:39:12 +0300

    addapted the Cmkec to box2d

--- include/Board.h
+++ include/Board.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+#include "box2d/box2d.h"
 #include "Macros.h"
 
 #include "playersInclude/Light.h"
@@ -19,6 +20,9 @@ public:
 
 private:
 	int m_level;
+
+	
+	b2BodyDef m_groundBodyDef;		// Define the ground body
 
 	Light m_lightPlayer;
 	Heavy m_heavyPlayer;
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,13 @@
 #include "Board.h"
 
+const b2Vec2 gravity = b2Vec2(0.0f, 10.0f);
 
 Board::Board() 
-{}
+{
+	// Construct a world object, which will hold and simulate the rigid bodies.
+	// Use dynamic allocation as this is a big object, and allocating it on the stack
+	auto world = std::make_unique<b2World>(gravity);
+
+	m_groundBodyDef.position.Set(0.0f, -10.0f);
+
+}
