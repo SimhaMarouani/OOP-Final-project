ad5b76d80c96fefbf407cc32b19b55b82e1340f2
Simha Marouani <simhama@std.hac.ac.il>
2022-05-23 00:43:14 +0300

    .

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,14 +3,10 @@
 
 Board::Board() 
 {
-
-	
-
 	//init players positions (function in gameObject?)
-	m_heavyPlayer.setPostition(sf::Vector2f(300, 300));
-	m_lightPlayer.setPostition(sf::Vector2f(500, 300));
-	m_simplePlayer.setPostition(sf::Vector2f(1000, 300));
-
+	m_heavyPlayer.setPostition(sf::Vector2f(300, 600));
+	m_lightPlayer.setPostition(sf::Vector2f(500, 600));
+	m_simplePlayer.setPostition(sf::Vector2f(1000, 600));
 }
 
 void Board::draw(sf::RenderWindow& window)
@@ -18,5 +14,9 @@ void Board::draw(sf::RenderWindow& window)
 	m_heavyPlayer.draw(window);
 	m_lightPlayer.draw(window);
 	m_simplePlayer.draw(window);
-
 }
+
+//void Board::createObject(Controller& controller)
+//{
+//	controller.createPlayersVec();
+//}
