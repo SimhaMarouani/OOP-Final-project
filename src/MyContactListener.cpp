1fe699020cfa0cb071260b5cd1bac8a8a134e8e8
Tali Kalev <talkalev@gmail.com>
2022-06-08 20:27:56 +0300

    sheep now jump based on foot sensor collision

--- src/MyContactListener.cpp
+++ src/MyContactListener.cpp
@@ -1,26 +1,30 @@
 #include "MyContactListener.h"
 
 void MyContactListener::BeginContact(b2Contact* contact) {
+
     //check if fixture A was the foot sensor
     void* bodyUserDataA = contact->GetFixtureA()->GetBody()->GetUserData();
     auto objA = static_cast<GameObjects*>(bodyUserDataA);
     void* fixtureUserDataA = contact->GetFixtureA()->GetUserData();
-   
+
+    //check if fixture B was the foot sensor
     void* bodyUserDataB = contact->GetFixtureB()->GetBody()->GetUserData();
     auto objB = static_cast<GameObjects*>(bodyUserDataB);
     void* fixtureUserDataB = contact->GetFixtureB()->GetUserData();
 
     if (!objA | !objB)
         return;
-
-
-    /*CollisionHandler::instance().processCollision(objA, objB,
-        ((int)fixtureUserDataA == 1) ? true : false, (int)fixtureUserDataB == 1) ? true : false);*/
-
-    CollisionHandler::instance().processCollision(objA, objB, true, true);
-
-
-
+    if ((int)fixtureUserDataA == 1)
+    {
+        std::cout << "fixture a is foot\n";
+    }
+    if ((int)fixtureUserDataB == 1)
+    {
+        std::cout << "fixture b is foot\n";
+    }
+    CollisionHandler::instance().processCollision(objA, objB, 
+        ((int)fixtureUserDataA == 1) ? true : false, 
+        ((int)fixtureUserDataB == 1) ? true : false);
 }
 
 void MyContactListener::EndContact(b2Contact* contact) {
