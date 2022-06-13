ed2d957f7ca28fc8b42eea8a0f413af740565130
Tali Kalev <talkalev@gmail.com>
2022-06-13 19:18:18 +0300

    turned rafter

--- src/objectsSrc/Rafter.cpp
+++ src/objectsSrc/Rafter.cpp
@@ -6,12 +6,12 @@ Rafter::Rafter(sf::Vector2f pos, b2World* world, sf::Vector2f scale)
 	getBody()->GetFixtureList()->SetSensor(false);
 	b2MassData mass;
 	mass.center = getBody()->GetLocalCenter();
-	mass.mass = 10;
-	//mass.I = m_icon.getOrigin().x;
+	mass.mass = 1;
+	mass.I = m_icon.getOrigin().y;
 	//mass.I = 400;
 	getBody()->SetMassData(&mass);
 
 	
-	//m_body->SetLinearDamping(0.1f);
-	//m_body->SetAngularDamping(0.1f);
+	m_body->SetLinearDamping(0.1f);
+	m_body->SetAngularDamping(0.1f);
 }
